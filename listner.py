7cea39f5db981ad74e6e8c46b1482e9740b93c0b
marispmei <94939570+marispmei@users.noreply.github.com>
2022-05-03 16:16:54 -0300

    Update listner.py

--- listner.py
+++ listner.py
@@ -6,17 +6,14 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     
 def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+    vetor_ang = (velocidade_ang_x**2+ velocidade_ang_y**2+velocidade_ang_z**2)**0.5
+    vetor_lin = (velocidade_lin_x**2+ velocidade_lin_y**2+velocidade_lin_z**2)**0.5
+    return vetor_ang
+    return vetor_lin
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber("vetores", String, callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
